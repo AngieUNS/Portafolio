528c65bf6099bd273823ec1d5dae2f6367cd3742
AngieUNS <202014055@uns.edu.pe>
2026-01-24 11:47:40 -0500

    Correcion de descarga a carpeta public

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 1. Importa esto
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 2. Agrégalo aquí
+    tailwindcss(),
   ],
+  publicDir: 'public', // 3. Asegura que Vite use esta carpeta
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
